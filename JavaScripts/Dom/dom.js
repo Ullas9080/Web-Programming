8fa913c2606799eb2943dd18fafeaf88a7d7cea6
Ullas M <116018377+Ullas9080@users.noreply.github.com>
2024-01-25 23:01:58 +0530

    Add files via upload

--- JavaScripts/Dom/dom.js
+++ JavaScripts/Dom/dom.js
@@ -1,9 +1,127 @@
-console.dir(document)
-console.log(document.child)
-//console.log(document.getElementById("name").innerHTML="congarts")
-// console.log(document.body.style.background="Blue")
-// console.log(document.getElementsByTagName("h1"))
-// let name=document.querySelector("h1")
-// console.log(name)
-//document.querySelector("h4").innerHTML="First step"
-document.querySelector("div").childNodes;
+let classes=document.getElementsByClassName("family")
+console.log(classes)
+let head=document.getElementsByClassName("head")
+console.log(head)
+let query=document.querySelectorAll(".heading")
+console.log(query)
+let tag=document.querySelectorAll('li')
+console.group(tag)
+let particular=document.querySelector('li:nth-child(2)')
+console.log(particular)
+let id=document.querySelector("#fruits")
+console.log(id)
+console.log(document.querySelector("p").innerHTML)
+console.log(document.querySelector("p").innerHTML="<h1>Js array method</h1>")
+console.log(document.querySelector("p:nth-child(3)").innerHTML="<h1>filter</h1>")
+console.log(`Inner Text :${document.querySelector(".text").innerText}`)
+console.log(`Inner HTML :${document.querySelector(".text").innerHTML}`)
+console.log(`Text Content :${document.querySelector(".text").textContent}`)
+console.log(document.querySelector(".p1").innerHTML)
+console.log(document.querySelector(".p1").innerText)
+console.log(document.querySelector(".p1").textContent)
+console.log(document.querySelector(".p2").innerHTML)
+console.log(document.querySelector(".p2").innerText)
+console.log(document.querySelector(".p2").textContent)
+const address = `
+  ABC Street,
+  Spintex Road,
+  Accra,
+  Ghana.
+`;
+const address2=`
+Basavangudhi,
+Byrappa Block,
+thyagarajnagar,
+bangalore.
+`
+console.log(document.querySelector("#inner-text").innerText=address)
+console.log(document.querySelector("#textContent").innerText=address2)
+console.log(document.querySelector('img').getAttribute('alt'))
+console.log(document.querySelector('img').getAttribute('src'))
+const image=document.querySelector('img')
+//Changing image attribute
+console.log(`image attribute Before ${image.getAttribute('src')}`)
+//Changing image attribute
+image.setAttribute('src','virat1.jpg')
+console.log(`image attribute After ${image.getAttribute('src')}`)
+const classes=document.querySelector('p')
+console.log(`get Attribute ${classes.getAttribute('class')}`)
+classes.setAttribute('class','Paragaraph1')
+console.log(`Set Attribute ${classes.getAttribute('class')}`)
+console.log(classes.getAttribute('id'))
+classes.setAttribute('id','Paragraph1')
+console.log(classes.getAttribute('id'))
+console.log(classes.removeAttribute('id'))
+console.log(classes.hasAttribute('id'))
+let list=document.querySelector("p")
+console.log(list.classList)
+//add
+list.classList.add('Abd')
+console.log(list.classList)
+//remove
+list.classList.remove('Abd')
+console.log(list.classList)
+//replace
+list.classList.replace('virat','rcb')
+console.log(list.classList)
+//contain
+console.log(list.classList.contains('rcb'))
+console.log(list.classList.contains('abd'))
+//Toggle
+list.classList.toggle('rcb')
+console.log(list.classList)
+list.classList.toggle('rcb')
+console.log(list.classList)
+list.classList.toggle('rcb')
+console.log(list.classList)
+let pp=document.querySelector('#italics')
+console.log(pp.parentNode.parentElement.parentElement)
+let cc=document.getElementById('container')
+console.log(cc.childNodes)
+console.log(cc.children)
+console.log(cc.firstChild)
+console.log(cc.lastChild)
+console.log(cc.firstElementChild)
+console.log(cc.lastElementChild)
+let first=document.querySelector('.first')
+first.onclick=(function numc(num)
+{
+sum=num+1
+console.logs(sum)
+return sum
+}
+numc(1))
+function sum(num1,num2){
+  console.log(num1+num2)
+}
+function display(){
+  let num1=Number(prompt("Enter first value"))
+  let num2=Number(prompt("Enter second value"))
+  sum(num1,num2)
+}
+let first=document.querySelector('.first')
+first.onmouseup=function(){
+  display()
+}
+let h2=document.querySelector('h2')
+console.log(h2.append("From Ullas gowda"))
+let box=document.querySelectorAll(".box")
+console.log(box)
+console.log(box[1])
+box[2].innerHTML ="New unique"
+for(div of box)
+{
+  console.log(box)
+}
+let box=document.querySelectorAll('div')
+let idx=1
+for (values of box)
+{
+ values.innerHTML=`New Box ${idx}`
+ idx++
+}
+let repeat_box=document.querySelector('div')
+for (let i=0;i<5;i++)
+{
+  console.log(repeat_box)
+}
